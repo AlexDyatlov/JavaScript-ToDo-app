fd16d79eae09a84dafde8c72e05c7f9852cae58d
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-28 09:06:31 +0300

    part 2

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const taskList = document.querySelector('#list-group');
 const emptyListItem = document.querySelector('#empty-list-item')
 
 // 1. Добавление новой задачи
-
 // Отслеживание отправки формы
 form.addEventListener('submit', function (e) {
   // Отменяем стандартноеповедение при отправке формы (перезагрузку страницы)
@@ -34,6 +33,38 @@ form.addEventListener('submit', function (e) {
 
   // возвращаем фокус на поле ввода
   input.focus();
+})
+
+taskList.addEventListener('click', function(e) {
+  // console.log(e.target)
+
+  // Проверяем что клик произошел по кнопке 'удалить'
+  if (e.target.getAttribute('data-action') == 'delete-task') {
+    
+    // Находим родительский тег <li> и уадляем его
+    e.target.closest('.list-group-item').remove();
+
+    toggleEmptyListItem();
+  } else if (e.target.getAttribute('data-action') == 'ready') {
+  
+    // Находим родительский тег <li>
+    const parentElement = e.target.closest('li.list-group-item');
+
+    // добавляем к тегу span доп. класс
+    parentElement.querySelector('.task-title').classList.add('task-title--done')
+
+    // убираем у тега span атрибут contenteditable
+    parentElement.querySelector('.task-title').setAttribute('contenteditable', 'false')
+
+    // Перемещаем запись в конец списка
+    taskList.insertAdjacentElement('beforeend', parentElement);
+
+    // удалить кнопку 'готово' и 'удалить'
+    parentElement.querySelector('button[data-action="ready"]').remove();
+    parentElement.querySelector('button[data-action="delete-task"]').remove();
+
+  }
+  
 })
 
 // скрываем "список дел пуст"
